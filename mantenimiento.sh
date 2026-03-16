0d030f9efb5e021100cad631612f82a3def21934
eriikmp <erikmapa06@gmail.com>
2026-03-16 18:26:49 +0100

    Nuevo mantenimiento

--- mantenimiento.sh
+++ mantenimiento.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 mkdir -p /home/eriikmp/backups
-sudo tar -czf /home/eriikmp/backups/copia.tar.gz /etc/nginx /home/eriikmp
+sudo tar --exclude='/home/eriikmp/practica_1/backups'\ --exclude='/home/eriikmp/practica_1/.git'\ -czf /home/eriikmp/backups/copia.tar.gz /etc/nginx /home/eriikmp
 sudo find /var/log -name "*.log" -type f -mtime +7 -delete
 echo "Mantenimiento completado"
+
+cd ~/practica_1
+git add backups
+git commit -m "Nueva copia de seguridad"
+git push
